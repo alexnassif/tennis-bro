3985b7655160ade63c8e1fc134317efaab0777a3
alex nassif <anassif001@gmail.com>
2021-07-19 18:07:43 -0700

    compare passwords

--- Models/User.go
+++ Models/User.go
@@ -15,27 +15,34 @@ func GetAllUsers(users *[]User) (err error) {
 }
 //CreateUser ... Insert New data
 func CreateUser(user *User) (err error) {
- if err = Config.DB.Create(user).Error; err != nil {
+ if err = Config.DB.Create(&user).Error; err != nil {
   return err
  }
  return nil
 }
 //GetUserByID ... Fetch only one user by Id
 func GetUserByID(user *User, id string) (err error) {
- if err = Config.DB.Where("id = ?", id).First(user).Error; err != nil {
+ if err = Config.DB.Where("id = ?", id).First(&user).Error; err != nil {
   return err
  }
  return nil
 }
+
+func FindUserByUsername(username string, dbUser *User) (err error){
+    if err = Config.DB.Where("user_name = ?", username).First(&dbUser).Error; err != nil {
+        return err
+    }
+    return nil
+}
 //UpdateUser ... Update user
 func UpdateUser(user *User, id string) (err error) {
  fmt.Println(user)
- Config.DB.Save(user)
+ Config.DB.Save(&user)
  return nil
 }
 //DeleteUser ... Delete user
 func DeleteUser(user *User, id string) (err error) {
- Config.DB.Where("id = ?", id).Delete(user)
+ Config.DB.Where("id = ?", id).Delete(&user)
  return nil
 }
 
@@ -55,7 +62,7 @@ func GetAllOnlineUsers(users *[]OnlineClient) (err error) {
 
 func RemoveOnlineUser(user *OnlineClient) (err error) {
 
-    Config.DB.Where("id = ?", user.GetId()).Delete(user)
+    Config.DB.Where("id = ?", user.GetId()).Delete(&user)
     return nil
 
 }
---
+++ api.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alexnassif/tennis-bro/Auth"
+	"github.com/alexnassif/tennis-bro/Models"
+)
+
+type LoginUser struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+}
+
+
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+
+    var loginUser LoginUser
+
+    // Try to decode the JSON request to a LoginUser
+    err := json.NewDecoder(r.Body).Decode(&loginUser)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+	var dbUser Models.User
+    // Find the user in the database by username
+    userErr := Models.FindUserByUsername(loginUser.Username, &dbUser)
+    if userErr != nil {
+        returnErrorResponse(w)
+        return
+    }
+
+    // Check if the passwords match
+    ok, err := Auth.ComparePassword(loginUser.Password, dbUser.Password)
+
+    if !ok || err != nil {
+        returnErrorResponse(w)
+        return
+    }
+
+    // Create a JWT
+    token, err := Auth.CreateJWTToken(dbUser)
+
+    if err != nil {
+        returnErrorResponse(w)
+        return
+    }
+
+    w.Write([]byte(token))
+
+}
+
+func returnErrorResponse(w http.ResponseWriter) {
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write([]byte("{\"status\": \"error\"}"))
+}
