0806e7c1276215dbe2f2ac52400d27be25d2dad0
alex nassif <anassif001@gmail.com>
2021-07-19 21:01:10 -0700

    jwt

---
+++ Auth/jwt.go
@@ -0,0 +1,58 @@
+// auth/jwt.go
+package Auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/alexnassif/tennis-bro/Models"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const hmacSecret = "SecretValueReplaceThis"
+const defaulExpireTime = 604800 // 1 week
+
+type Claims struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+func (c *Claims) GetId() string {
+	return c.ID
+}
+
+func (c *Claims) GetName() string {
+	return c.Name
+}
+
+// CreateJWTToken generates a JWT signed token for for the given user
+func CreateJWTToken(user Models.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Id":        user.GetId(),
+		"Name":      user.GetName(),
+		"ExpiresAt": time.Now().Unix() + defaulExpireTime,
+	})
+	tokenString, err := token.SignedString([]byte(hmacSecret))
+
+	return tokenString, err
+}
+
+func ValidateToken(tokenString string) (Models.User, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(hmacSecret), nil
+	})
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/alexnassif/tennis-bro
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v8 v8.10.0
 	github.com/golang/protobuf v1.4.2 // indirect
