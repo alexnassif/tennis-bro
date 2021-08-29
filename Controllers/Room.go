dff3941bfc1009b03e5506ffbd4e281302ed8052
alex nassif <anassif001@gmail.com>
2021-08-28 22:05:32 -0700

    room auth

--- Controllers/Room.go
+++ Controllers/Room.go
@@ -8,6 +8,15 @@ import(
 
 func GetRoomsForUser(c *gin.Context) {
 	id := c.Params.ByName("id")
+
+	usertok, ok := c.Keys["user"].(Models.LoggedInUser)
+	if !ok {
+		return
+	}
+
+	if usertok.GetId() != id {
+		return
+	}
 	var privateRooms []Models.Room
 	err := Models.GetRoomsByUsers(id, &privateRooms)
 	if err != nil {
