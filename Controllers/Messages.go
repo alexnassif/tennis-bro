59372632cb42e6de9f53ac8fff80889421774e41
alex nassif <anassif001@gmail.com>
2021-08-28 22:04:08 -0700

    messages auth

--- Controllers/Messages.go
+++ Controllers/Messages.go
@@ -9,6 +9,16 @@ import(
 func GetMessagesByUserId(c *gin.Context) {
 	sender := c.Params.ByName("sender")
 	recipient := c.Params.ByName("recipient")
+
+	usertok, ok := c.Keys["user"].(Models.LoggedInUser)
+	if !ok {
+		return
+	}
+
+	if usertok.GetId() != sender {
+		return
+	}
+
 	var messages []Models.Message
 	err := Models.GetMessagesByUser(sender, recipient, &messages)
 	if err != nil {
